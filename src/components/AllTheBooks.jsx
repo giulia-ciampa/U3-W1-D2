c04afef1f0ac3cb7add6c8b62eb9ef165b2ddd64
giulia-ciampa <giulia.ciampa7@gmail.com>
2026-04-28 17:11:45 +0200

    piccoli aggiustamenti

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -4,19 +4,19 @@ import { Card, Button, Container, Row, Col } from "react-bootstrap"
 
 const AllTheBooks = function () {
   return (
-    <Container>
-      <Row className="justify-content-center g-3">
+    <Container className="my-4">
+      <Row className="justify-content-start g-3">
         {books.map((oneBook) => {
           return (
             <Col sm={12} md={6} lg={3} key={oneBook.asin}>
               <Card className="h-100">
                 <Card.Img variant="top" src={oneBook.img} />
                 <Card.Body className="d-flex flex-column">
-                  <Card.Title className="mt-auto mb-auto">
+                  <Card.Title className="mt-auto mb-auto fs-6">
                     {oneBook.title}
                   </Card.Title>
-                  <Card.Text>
-                    {oneBook.category} - {oneBook.price}&euro;
+                  <Card.Text className="mt-1 d-flex justify-content-between">
+                    <p>{oneBook.category}</p> <p>{oneBook.price}&euro;</p>
                   </Card.Text>
                   <Button variant="danger" className="mt-auto">
                     Aggiungi al carrello
